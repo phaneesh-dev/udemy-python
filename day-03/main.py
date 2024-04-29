870ca00fa02acfead4e30278416c7496544661c7
phaneesh-dev <developersreephaneesha@gmail.com>
2024-04-29 15:52:47 +0000

    completed day-03

---
+++ day-03/main.py
@@ -0,0 +1,47 @@
+# compasirion oprator
+
+print(2 == 3)  # False
+print(2 == 2)  # True
+print(2 > 1)  # True
+print(2 > 4)  # False
+print(2 < 4)  # True
+print(10 < 4)  # False
+print(10 <= 4)  # False
+print(10 >= 4)  # True
+print(10 != 4)  # True
+print(4 != 4)  # False
+
+
+print()
+print()
+print()
+
+# logical oprator
+print(2 == 2 and 3 == 3)  # True
+print(2 == 3 or 3 == 3)  # True
+print(not 3 == 3)  # False
+
+'''
+and 
+
+true and true --> true
+true and false --> false  
+fasle and true --> false  
+fasle and false --> false  
+'''
+
+'''
+or
+
+true and true --> true
+true and false --> true  
+fasle and true --> true  
+fasle and false --> false  
+'''
+
+'''
+not
+
+true --> false
+false --> true  
+'''
